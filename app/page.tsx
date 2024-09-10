38e28a86f16b6c848d52486a406db4e89c1801f9
Mohanad Osman <34104848+MoHoThrives@users.noreply.github.com>
2024-09-10 11:40:50 -0400

    Completed the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,45 @@
 // import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
 import Banner from "@/components/home/banner";
 import Header from "@/components/home/header";
+import HowItWorks from "@/components/home/howitworks";
+import Pricing from "@/components/home/pricing";
 import { Dot } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="
+    <main
+      className="
     mx-auto w-full inset-0 h-full
-    bg-[radial-gradient(#fdf5b7_1px), transparent_1px)]
-    [background-size:16px_16px]">
+    bg-[radial-gradient(#fdf5b7_1px, transparent_1px)]
+    [background-size:16px_16px]"
+    >
       {/* <div className="bg-gradient-to-br from-yellow-300 via-white to-white"></div> */}
-      <Header/>
-      <Banner/>
+      <BgGradient />
+      <Header />
+      <Banner />
       <div className="flex items-center justify-center">
         <Dot className="text-orange-400"></Dot>
         <Dot className="text-orange-400"></Dot>
         <Dot className="text-orange-400"></Dot>
       </div>
-      { /* < HowItWorks/>
-      <Divider/>
-      <Pricing/>
-      <Divider/>
-      <Footer/> */}
+      <HowItWorks />
+      <div className="flex items-center justify-center">
+        <Dot className="text-orange-400"></Dot>
+        <Dot className="text-orange-400"></Dot>
+        <Dot className="text-orange-400"></Dot>
+      </div>
+      <Pricing />
+      <div className="flex items-center justify-center">
+        <Dot className="text-orange-400"></Dot>
+        <Dot className="text-orange-400"></Dot>
+        <Dot className="text-orange-400"></Dot>
+      </div>
+      <footer className="bg-gray-200/20 flex h-20 py-24 px-12 z-20 relative overflow-hidden">
+        <p>All rights reserved {new Date().getFullYear()}</p>
+        <p className="flex lg:justify-end lg:flex-1"><a href="https://mohanad.dev" target="_blank">Built by Mohanad 👨‍💻</a></p>
+      </footer>
     </main>
   );
 }
-
-// bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] 
-//     from-red-200 via-yellow-200 to-yellow-300
