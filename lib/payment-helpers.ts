8eea01cc2d4247461a27b9e351f777cbfcc445b9
Mohanad Osman <34104848+MoHoThrives@users.noreply.github.com>
2024-09-15 23:45:39 -0400

    Finished the payment insertion function. Started handling subscription deletion

--- lib/payment-helpers.ts
+++ lib/payment-helpers.ts
@@ -2,6 +2,20 @@ import Stripe from "stripe";
 import getDbConnection from "./db";
 import { NeonQueryFunction } from "@neondatabase/serverless";
 
+export async function handleSubscriptionDeleted({
+  subscriptionId,
+  stripe,
+}: {
+  subscriptionId: string;
+  stripe: Stripe;
+}) {
+    try {
+        
+    } catch (err) {
+        console.log("Error removing subscription")
+    }
+}
+
 export async function handleCheckoutSessionCompleted({
   session,
   stripe,
@@ -16,9 +30,23 @@ export async function handleCheckoutSessionCompleted({
     const sql = await getDbConnection();
     await createOrUpdateUser(sql, customer, customerId);
     //Updating the user
-    await updateUserSubscription(sql, priceId, customer.email as string)
+    await updateUserSubscription(sql, priceId, customer.email as string);
     //Insert payment
-    await insertPayment
+    await insertPayment(sql, session, customer.email as string, priceId);
+  }
+}
+
+async function insertPayment(
+  sql: NeonQueryFunction<false, false>,
+  session: Stripe.Checkout.Session,
+  email: string,
+  priceId: string
+) {
+  try {
+    await sql`INSERT INTO payments (amount, status, stripe_payment_id, user_email, price_id)
+        VALUES (${session.amount_total}, ${session.status}, ${session.id}, ${email}, ${priceId})`;
+  } catch (err) {
+    console.log("Error inserting payment");
   }
 }
 
